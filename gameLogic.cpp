12aef0de3b62a0da02d975aa3df79c1d810e3af3
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2019-04-09 18:46:44 -0700

    Create gameLogic.cpp

---
+++ gameLogic.cpp
@@ -0,0 +1,6 @@
+// *********** CREATE EVENT **********
+randomize();      // ensures "random" functions appear random at every run of the game kinda like "srand(time(0));" in c++
+room_goto_next(); // goes to the next room of the game 
+
+// *********** STEP EVENT **********
+if(keyboard_check_pressed(ord("R"))){ game_restart(); }
