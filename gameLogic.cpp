e5bed5a68896fc09ce4223c98f1d101620022f3f
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2019-04-09 18:48:06 -0700

    Update gameLogic.cpp
    Most of our system/basic game logic will go here that the player never actually sees or interacts with

--- gameLogic.cpp
+++ gameLogic.cpp
@@ -3,4 +3,4 @@ randomize();      // ensures "random" functions appear random at every run of th
 room_goto_next(); // goes to the next room of the game 
 
 // *********** STEP EVENT **********
-if(keyboard_check_pressed(ord("R"))){ game_restart(); }
+if(keyboard_check_pressed(ord("R"))){ game_restart(); } // mostly for debugging so we can restart the game while it's running 
