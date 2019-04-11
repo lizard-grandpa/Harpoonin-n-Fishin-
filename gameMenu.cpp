3b3125c3e1b12da1271907d3dec102dc15c9ac7a
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2019-04-10 18:57:13 -0700

    Create gameMenu.cpp

---
+++ gameMenu.cpp
@@ -0,0 +1,55 @@
+// ******* CREATE EVENT ******
+menuX = x;
+menuY = y;
+buttonHeight = 64;
+
+// ------- create menu buttons array 
+button[0] = "Start";
+button[1] = "Skins Store";
+button[2] = "Power Ups Store";
+button[3] = "Exit";
+
+buttons = array_length_1d(button); // store the length of the 'button' array 
+
+menuIndex = 0;
+lastSelected = 0; 
+
+// ******* STEP EVENT ********
+menuMove = keyboard_check_pressed(vk_down) - keyboard_check_pressed(vk_up);
+
+menuIndex += menuMove;
+if(menuIndex < 0)				{ menuIndex = buttons - 1; }
+if(menuIndex > (buttons - 1))	{ menuIndex = 0; }
+
+// if(menuIndex != lastSelected){ audio_play_sound(soundName, 1, false); }
+// if we decide to use audio for each button switch ^ also "false" can be changed to "true" if we want it to loop
+
+lastSelected = menuIndex;
+
+// ******** DRAW EVENT ********
+var i = 0;
+
+repeat(buttons){
+	draw_set_font(f_1);         // f_1 the name i chose for a custom font that can be changed, font size is 32
+	draw_set_halign(fa_center); // centers the text 
+	draw_set_color(c_teal);     // sets the color of the font 
+	if(menuIndex == i)	{ draw_set_color(c_black); }
+	draw_text(menuX, menuY + (buttonHeight * i) , button[i]);
+	i++
+}
+
+// ******** KEY PRESS EVENT - ENTER KEY ********
+switch(menuIndex){
+	case 0:
+		room_goto(rm_game); // will go directly to rm_demo after
+		break;
+	case 1: 
+		room_goto(rm_skinsStore);
+		break;
+	case 2: 
+		room_goto(rm_powerUpsStore);
+		break;
+	case 3: 
+		game_end(); // closes the game window
+		break;
+}
