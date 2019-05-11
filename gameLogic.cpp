dd0791a188cada8783c638bad1ef89acf29d9658
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2019-05-11 10:41:10 -0700

    Update gameLogic.cpp

--- gameLogic.cpp
+++ gameLogic.cpp
@@ -4,3 +4,12 @@ room_goto_next(); // goes to the next room of the game
 
 // *********** STEP EVENT **********
 if(keyboard_check_pressed(ord("R"))){ game_restart(); } // mostly for debugging so we can restart the game while it's running 
+
+// ---- creating fish instances
+var fish_count = instance_number(obj_fish);
+fish_x = choose(-120, 950);
+
+if(fish_count < 10)
+{ 
+	instance_create_depth(fish_x, irandom_range(0, 765), 100, obj_fish); // x, y, depth, object
+}
