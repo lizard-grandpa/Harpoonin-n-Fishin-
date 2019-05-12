a3c892cc16832a71e0da9cf26f6c641223661e52
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2019-05-11 17:13:06 -0700

    Update obj_harpoon.cpp

--- obj_harpoon.cpp
+++ obj_harpoon.cpp
@@ -6,7 +6,7 @@ score += 10;
 instance_destroy();
 
 // **** COLLISION EVENT W/ OBJ_OIL_BARREL
-instance_create_depth(view_xport[0], view_yport[0], -100, obj_oil_spill);
+instance_create_depth(view_xport[0] + 200, view_yport[0] - 500, -100, obj_oil_spill);
  
 // **** OUTSIDE ROOM EVENT ****
 instance_destroy();
