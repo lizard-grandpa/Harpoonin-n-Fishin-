81f318109b8f6b4690a527bd417e88ce202bcba6
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2019-05-14 17:38:19 -0700

    Update obj_harpoon.cpp

--- obj_harpoon.cpp
+++ obj_harpoon.cpp
@@ -1,14 +1,15 @@
 // **** CREATE EVENT ****
-speed = 6;
+speed = 6; // harpoon speed 
 
 // **** STEP EVENT ****
+
 // ------- POSITION VARIABLES 
 xPosition = 0;
 yPosition = 0;
 
 xPosition += speed;
 yPosition += speed;
-// ------- COLLISION CHECK WITH OBJECTS
+// ------- COLLISION CHECK WITH OBJECTS | source for collision code: FriendlyCosmoNaut 
 // horizontal
 barrel_collision = false;
 
@@ -16,7 +17,7 @@ if(xPosition != 0){
 	if(place_meeting(x + xPosition, y, obj_oil_barrel)){ // checks if craboo's x & y position collides w/ obj_rock's collision mask
 		repeat(abs(xPosition)){					   // repeating for the absolute value amount of xPosition 
 			if(!place_meeting(x + sign(xPosition), y ,obj_oil_barrel)) { x += sign(xPosition); } 
-			else{ break; }						   //  loop ends once craboo's intended xPosition + or - 1, collides w/ obj_rock
+			else{ break; }						   //  loop ends once diver's intended xPosition + or - 1, collides w/ obj_rock
 		}
 		xPosition = 0;							   // x movement is stopped  
 	}
@@ -26,7 +27,7 @@ if(yPosition !=0){
 	if(place_meeting(x, y + yPosition, obj_oil_barrel)){
 		repeat(abs(yPosition)){
 			if(!place_meeting(x, y + sign(yPosition), obj_oil_barrel)) { y += sign(yPosition); }
-			else { break; } //  loop ends once craboo's intended yPosition + or - 1, collides w/ obj_rock
+			else { break; } //  loop ends once diver's intended yPosition + or - 1, collides w/ obj_rock
 		}
 		yPosition = 0;      // y movement is stopped 
 		barrel_collision = true;
@@ -34,13 +35,14 @@ if(yPosition !=0){
 }
 
 if(barrel_collision){ 
+	score -= 15;
 	instance_create_depth(view_xport[0]+ 200, view_yport[0]- 500, -100, obj_oil_spill);
 	instance_destroy(obj_harpoon);
 	audio_play_sound(Barrel, 10, false);
 	
 }
 // **** COLLISION EVENT W/ OBJ_FISH ****
-score += 10;
+audio_play_sound(s_fish_hit, 1, false);
 instance_destroy();
  
 // **** OUTSIDE ROOM EVENT ****
